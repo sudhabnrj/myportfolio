a879f9425a14fbcf48cac5242ec329598e037125
Sudhabnrj <sudhabnrj@gmail.com>
2024-05-11 20:30:46 +0530

    work page fixed

--- src/components/WorkExpList.js
+++ src/components/WorkExpList.js
@@ -1,7 +1,7 @@
 const WorkExpList = ({designation, companyName, summary, joiningDate, lastWorkingDate, currentlyWorking}) => {
     return(
         <li className="flex flex-col relative pl-4 lg:pl-20 before:content-[''] before:absolute before:left-[-4px] before:w-2 before:h-2 before:rounded-full before:top-0 before:bg-black dark:before:bg-slate-700 mb-14">
-            <h2 className="mb-4 flex justify-between items-center">
+            <h2 className="mb-4 flex flex-col sm:flex-row justify-between sm:items-center">
                 <span className="font-bold text-xl">{designation}</span>
                 <span className="text-slate-700 dark:text-slate-300">
                     {joiningDate} {lastWorkingDate ? `- ${lastWorkingDate}` : ''}
